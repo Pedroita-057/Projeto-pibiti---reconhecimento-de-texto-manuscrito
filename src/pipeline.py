e050f23db21c2343d0bb9ec4837db6f6fe20032b
Pedroita-057 <ps0570732@gmail.com>
2026-02-17 11:27:28 -0300

    ediçao

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,16 @@
+# Aqui são todas as importações
+
+# aqui é o redimenensionamento, normalização, remoção de ruído da iamgem.
 from preprocessing.image_preprocessing import preprocess_image
+
+#nessa fase carrega o modelo treinado
 from model.htr_model import recognize_text
+
+#aqui é a função que carrega a métrica de avaliação
 from evaluation.metrics import evaluate
 
+
+#os caminhos para os arquivos de entrada e saída
 raw_image = "data/raw/exemplo.png"
 processed_image = "data/processed/exemplo.png"
 reference_text_path = "data/annotations/exemplo.txt"
